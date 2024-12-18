ae8e3141546673c7eadc1e6caf8ad0bf04f941e1
cam <patinas.0.golf@icloud.com>
2024-12-18 14:02:59 -0500

    Create health.ts

---
+++ api/health.ts
@@ -0,0 +1,10 @@
+export const config = {
+  runtime: 'edge',
+};
+  
+export default function handler(): Response {
+  return new Response('GTAPoliceMods API Running!', {
+    status: 200,
+    headers: { 'Content-Type': 'text/plain' },
+  });
+}
