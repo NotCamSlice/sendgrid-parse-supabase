2f436ed4263c02cef450e91e8520a06780639e64
cam <patinas.0.golf@icloud.com>
2024-12-18 20:28:41 +0000

    fix again again

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Enables strict mode for React
-    swcMinify: true,       // Use SWC for faster minification
-    env: {
-      SUPABASE_URL: process.env.SUPABASE_URL,       // Pass SUPABASE_URL to the client
-      SUPABASE_KEY: process.env.SUPABASE_KEY,       // Pass SUPABASE_KEY to the client
-      WEBHOOK_SECRET: process.env.WEBHOOK_SECRET,   // Pass WEBHOOK_SECRET to the client
-    },
-    experimental: {
-      runtime: 'edge', // Enable experimental Edge Runtime for Edge API routes
-    },
+  reactStrictMode: true, // Enable React strict mode
 };
-  
+
 module.exports = nextConfig;
-  
